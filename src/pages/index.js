7f79fc6d1380600ae65051fed97ce8230c706244
Andy Lobel <me@andylobel.com>
2019-06-03 16:34:41 +0100

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = ({data}) => (
+  <Layout>
+    {data.contentfulCourse.title}
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something greattttt.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+
+export default IndexPage
+
+export const query = graphql`
+  {
+    contentfulCourse {
+      title
+    }
+  }
+`
